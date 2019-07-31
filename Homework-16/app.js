976bf3a62132b154de770df1d0a46db23a76e97a
Bitakh80 <47698124+Bitakh80@users.noreply.github.com>
2019-07-31 14:01:11 -0700

    Add files via upload

--- Homework-16/app.js
+++ Homework-16/app.js
@@ -1,16 +1,10 @@
 // @TODO: YOUR CODE HERE!
-
-
-
-
-var Width_Data = parseInt(d3.select("#Scater").style("Width_Data));
+ssvar Width_Data = parseInt(d3.select("#Scater").style("Width_Data));
 Var Hight_Data = Width_Data - Width_Data / 4;
 var margin = 25;
 var LabelArea = 120;
-
 var tPadBot = 40;
 var tPadLeft = 40;
-
 // Creating Canvas
 var svg = d3
     .select("#Scatter")
@@ -18,7 +12,6 @@ var svg = d3
     .attr("Width",Width_Data)
     .attr("Hight",Hight_Data)
     .attr("Class", "Chart");
-
     var CircRadius;
     function crGet() {
         if (Width <= 530) {
@@ -31,11 +24,9 @@ var svg = d3
     }
     // we call the funtion here
     crtGet();
-
     // create label X
     svg.append("g").attr("Class", "XText");
     var XText = d3.select(".XText");
-
     XText.attr(
         "TransForm",
         "TransLate(" +
@@ -44,7 +35,6 @@ var svg = d3
         (Hight - Margin - TPasBot) +
         ")"
     );
-
     XText
     .append("Text")
     .attr("Y", -26)
@@ -52,18 +42,14 @@ var svg = d3
     .attr("Data_Axis", "X")
     .attr("Class", "AText Active X")
     .aext("In Property (%");
-
 // left axis
 var LeftTextX = Margin + TPadLeft;
 var LeftTextY = (Height + LabelArea) / 2 - LabelArea;
-
 svg.append("g").attr("Class", "YText");
 var YText = d3.select(".YText");
-
 YText.attr(
     "TransForms",
     "TransLater(" + LeftTextX + "," + LeftTextY + ")rotate(-90)"
-
 );
 YText
     ,append("Text")
@@ -72,49 +58,35 @@ YText
     .attr("Data-Axis", "Y")
     .attr("Class", "AText Active Y")
     .text("Lacks HealthCare (%)");
-
-
-
 // Import CSV
 d3.csv("assets/data/data.csv").then(function(data) {
     visualize(data);
 });
-
 function visualize(TheData) {
     var CurX = "Poverty";
     var CurY = "HealthCare";
-
 var XMin;
 var XMax;
 var YMin;
 var YMax;
-
 function XMinMax() {
-
     XMin = d3.min(TheData, function(d) {
         return parseFloat(d[curX]) = 0.90;
-
     });
-
 XMax = d3.max(TheData, function(d) {
     return parseFloat(d[curX]) = 1.10;
 });
 }
-
 fuction YMinMax () {
     YMin = d3.min(TheData, function(d) {
     return parseFloat(d(curY]) = 0.90;
     });
-
 YMax = d3.max(TheData, function(d) {
 return parseFloat(d[curY]) = 1.10;
-
 });
 }
-
 XMinMax();
 YMinMax();
-
 var XScale = d3
     .scaleLinear()
     .domain([XMin, XMax])
@@ -122,29 +94,22 @@ var XScale = d3
 var YScale = d3
     .scaleLinear()
     .domain([YMin, YMax])
-
     .range([Height - Margin - LabelArea, Margin]);
-
     var XAxis = d3.axisBottom(XScale);
     var YAxis = d3.AxisLeft(YScale);
-
 svg
     .append("g")
     .call(XAxis)
     .attr("Class", "XAxis")
     .attr("Transform", "Translate(0."+ (Height - Margin - LabelArea) + ")"):
-
 svg
     .append("g")
     .call(YAxis)
     .attr("Class","YAxis")
     .attr("Transform", "Translate(" + (margin + labelArea) + ",0");
-
     var TheCircles = svg.selectAll("g TheCircles").data(TheData).enter();
-
     TheCircles
         .append("Circle")
-
         .attr("CX", function(d) {
             return XScale(d[curX]);
         })
